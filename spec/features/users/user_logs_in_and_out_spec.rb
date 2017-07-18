ddb1864e1b14856f1872840c1a162559870ad68f
JF-Lalonde <lalondej88@gmail.com>
2017-07-18 15:17:57 -0600

    first commit

---
+++ spec/features/users/user_logs_in_and_out_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe "User logs in and out" do
+  scenario "with valid credentials" do
+    user_attributes = {
+      username: "Tester"
+      password: "password"
+    }
+  end
+end
